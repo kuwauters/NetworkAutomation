28d347de5ad44d658a9044c2af4ee090564b880d
kuwauters <kuwauters@gmail.com>
2017-02-12 22:00:52 +0100

    nother flash tryout

--- datamodel/epl_flask.py
+++ datamodel/epl_flask.py
@@ -7,10 +7,17 @@ js_file = 'inventory.json'
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
+
 def hello():
     json_data = json.load(open(work_dir + js_file))
     return render_template('epl.html',hosts=json_data)
 
+def contact():
+    if request.method=='POST':
+        if request.form['submit'] == 'build_epl':
+            print 'button works'
+        else:
+            print 'button does not work'
 if __name__ == "__main__":
     app.run()
